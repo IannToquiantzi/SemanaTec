cb518919960bdfa6c14ecceacf6071e6cdaf85a4
emisan <emisan031@gmail.com>
2023-05-11 21:07:48 -0600

    Se agrego la funcion para dibujar un recangulo

--- paint.py
+++ paint.py
@@ -41,8 +41,17 @@ def circle():
     t.circle(r)
     
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.x - start.x)
+
+    end_fill()
 
 
 def triangle(start, end):
