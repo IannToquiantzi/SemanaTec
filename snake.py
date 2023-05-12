b66c427daaec86e59e66fab33a677361a4773f20
emisan <emisan031@gmail.com>
2023-05-12 14:51:05 -0600

    Se agrega la funcion para mover la comida un paso a la vez

--- snake.py
+++ snake.py
@@ -10,8 +10,8 @@ Exercises
 
 from random import randrange
 from turtle import *
-
 from freegames import square, vector
+import random
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
@@ -23,11 +23,20 @@ def change(x, y):
     aim.x = x
     aim.y = y
 
-
+#Funcion para cambiar la direccion de movimiento de la comida:
+def change_food():
+    #Lista de vectores que corresponde a la direccion
+    food_list = [vector(0,10), vector(0,-10), vector(10,0), vector(-10,0)] 
+    #Eleccion al azar de la direccion de la comida
+    aim_food = random.choice(food_list)
+    #Ciclo para elegir otra direccion en caso de que la anterior salga de los limites:
+    while -200 > aim_food.x or  aim_food.x > 190 and -200 > aim_food.y or aim_food.y > 190: 
+        aim_food = random.choice(food_list) 
+    return aim_food   
+        
 def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
-
 
 def move():
     """Move snake forward one segment."""
@@ -50,13 +59,15 @@ def move():
 
     clear()
 
+    #Funcion para mover la comida de posicion:
+    food.move(change_food()) 
+
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
-
 
 setup(420, 420, 370, 0)
 hideturtle()
@@ -66,5 +77,6 @@ onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+change_food()
 move()
 done()
